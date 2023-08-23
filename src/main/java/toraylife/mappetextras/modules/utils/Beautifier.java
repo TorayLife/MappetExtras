62a2ea6aba39d34feba7e36f2a2d0c49239f103d
Toray-Life <stypmabichek@gmail.com>
2023-08-23 17:46:00 +0400

    Optimize imports

--- src/main/java/toraylife/mappetextras/modules/utils/Beautifier.java
+++ src/main/java/toraylife/mappetextras/modules/utils/Beautifier.java
@@ -12,10 +12,7 @@ import org.apache.http.message.BasicNameValuePair;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
