ca763a32a81d5b01c9cd897f84dc42365e08ba04
DESKTOP-30V720E\mines <jenya-vev@list.ru>
2023-09-02 19:14:16 +0300

    MixinScriptEntity: getFallDistance(), getForward()
    MixinScriptPlayer: add MixinTargetName
    
    en_US: add scripting module

--- src/main/java/toraylife/mappetextras/modules/scripting/mixins/MixinScriptEntity.java
+++ src/main/java/toraylife/mappetextras/modules/scripting/mixins/MixinScriptEntity.java
@@ -3,11 +3,13 @@ package toraylife.mappetextras.modules.scripting.mixins;
 import mchorse.mappet.CommonProxy;
 import mchorse.mappet.api.scripts.code.entities.ScriptEntity;
 import mchorse.mappet.api.scripts.code.entities.ScriptPlayer;
+import mchorse.mappet.api.scripts.user.data.ScriptVector;
 import mchorse.mappet.utils.RunnableExecutionFork;
 import mchorse.mclib.utils.Interpolation;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import toraylife.mappetextras.modules.main.documentation.MixinTargetName;
@@ -83,5 +85,13 @@ public abstract class MixinScriptEntity <T extends Entity>{
 
     public boolean isUndead(){
         return ((EntityLivingBase)this.entity).isEntityUndead();
+    }
+
+    public float getFallDistance(){
+        return this.entity.fallDistance;
+    }
+
+    public ScriptVector getForward(){
+        return new ScriptVector(this.entity.getForward().x, this.entity.getForward().y, this.entity.getForward().z);
     }
 }
