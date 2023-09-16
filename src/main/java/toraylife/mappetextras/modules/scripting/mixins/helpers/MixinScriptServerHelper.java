3e6555daeb9038587d09f17fa1ce383df8be5086
DESKTOP-30V720E\mines <jenya-vev@list.ru>
2023-09-16 23:23:35 +0300

    delete MixinScriptTileEntity, MixinScriptWorld, MixinScriptInventory
    delete all helpers

--- src/main/java/toraylife/mappetextras/modules/scripting/mixins/helpers/MixinScriptServerHelper.java
+++
@@ -1,6 +0,0 @@
-package toraylife.mappetextras.modules.scripting.mixins.helpers;
-
-import toraylife.mappetextras.modules.scripting.mixins.MixinScriptServer;
-
-public class MixinScriptServerHelper extends MixinScriptServer {
-}
