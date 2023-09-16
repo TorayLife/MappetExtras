9937e31088b84d6b760c91cc7a58a27f6d937b32
DESKTOP-30V720E\mines <jenya-vev@list.ru>
2023-09-16 23:27:22 +0300

    delete import

--- src/main/java/toraylife/mappetextras/modules/scripting/mixins/MixinScriptPlayer.java
+++ src/main/java/toraylife/mappetextras/modules/scripting/mixins/MixinScriptPlayer.java
@@ -10,7 +10,6 @@ import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import toraylife.mappetextras.modules.main.documentation.MixinTargetName;
-import toraylife.mappetextras.modules.scripting.mixins.helpers.MixinScriptFactoryHelper;
 
 import java.lang.reflect.Field;
 
