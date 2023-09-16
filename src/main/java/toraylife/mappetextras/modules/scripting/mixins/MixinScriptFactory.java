af1c59f472ed5e7719d08ef07ffc803c02099357
DESKTOP-30V720E\mines <jenya-vev@list.ru>
2023-09-16 23:24:43 +0300

    edit all methods MixinScriptFactory

--- src/main/java/toraylife/mappetextras/modules/scripting/mixins/MixinScriptFactory.java
+++ src/main/java/toraylife/mappetextras/modules/scripting/mixins/MixinScriptFactory.java
@@ -1,6 +1,10 @@
 package toraylife.mappetextras.modules.scripting.mixins;
 
 import mchorse.mappet.api.scripts.code.ScriptFactory;
+import mchorse.mappet.api.scripts.code.ScriptServer;
+import mchorse.mappet.api.scripts.code.ScriptWorld;
+import mchorse.mappet.api.scripts.code.blocks.ScriptTileEntity;
+import mchorse.mappet.api.scripts.code.items.ScriptInventory;
 import mchorse.mappet.api.scripts.user.IScriptServer;
 import mchorse.mappet.api.scripts.user.IScriptWorld;
 import mchorse.mappet.api.scripts.user.blocks.IScriptTileEntity;
@@ -10,26 +14,22 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import toraylife.mappetextras.modules.scripting.mixins.helpers.MixinScriptInventoryHelper;
-import toraylife.mappetextras.modules.scripting.mixins.helpers.MixinScriptServerHelper;
-import toraylife.mappetextras.modules.scripting.mixins.helpers.MixinScriptTileEntityHelper;
-import toraylife.mappetextras.modules.scripting.mixins.helpers.MixinScriptWorldHelper;
 
 @Mixin(value = ScriptFactory.class, remap = false)
 public abstract class MixinScriptFactory{
     public IScriptWorld getMappetWorld(World minecraftWorld) {
-        return new MixinScriptWorldHelper().create(minecraftWorld);
+        return new ScriptWorld(minecraftWorld);
     }
 
     public IScriptServer getMappetServer(MinecraftServer minecraftServer){
-        return new MixinScriptServerHelper().create(minecraftServer);
+        return new ScriptServer(minecraftServer);
     }
 
     public IScriptTileEntity getMappetTileEntity(TileEntity minecraftTileEntity){
-        return new MixinScriptTileEntityHelper().create(minecraftTileEntity);
+        return new ScriptTileEntity(minecraftTileEntity);
     }
 
     public IScriptInventory getMappetInventory(IInventory minecraftInventory){
-        return new MixinScriptInventoryHelper().create(minecraftInventory);
+        return new ScriptInventory(minecraftInventory);
     }
 }
