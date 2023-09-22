3341dbb8dd101cd67083cc665f2cb6ad81513cfe
Toray-Life <stypmabichek@gmail.com>
2023-09-22 14:36:30 +0400

    Remove "enabled" parameter in modules

--- src/main/java/toraylife/mappetextras/modules/utils/UtilsModule.java
+++ src/main/java/toraylife/mappetextras/modules/utils/UtilsModule.java
@@ -5,16 +5,16 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import toraylife.mappetextras.MappetExtras;
-import toraylife.mappetextras.modules.AbstractModule;
 import toraylife.mappetextras.modules.IModule;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class UtilsModule extends AbstractModule {
+public class UtilsModule implements IModule {
 
     private static UtilsModule instance;
+
     public static IModule getInstance() {
         if (UtilsModule.instance == null) {
             UtilsModule.instance = new UtilsModule();
@@ -25,34 +25,27 @@ public class UtilsModule extends AbstractModule {
     @Override
     public void addConfigOptions(ConfigBuilder builder) {
         builder.category("utils_module");
-        this.enabled = builder.getBoolean("enabled",true);
     }
 
     @Override
     public void preInit(FMLPreInitializationEvent event) {
-        if (!this.isEnabled()) return;
-
         MappetExtras.logger.info("Mappet extras utils module preInit!");
     }
 
     @Override
     public void init(FMLInitializationEvent event) {
-        if (!this.isEnabled()) return;
-
         MappetExtras.logger.info("Mappet extras utils module init!");
     }
 
     @Override
     public void postInit(FMLPostInitializationEvent event) {
-        if (!this.isEnabled()) return;
-
         MappetExtras.logger.info("Mappet extras utils module postInit!");
     }
 
     @Override
     public List<String> getMixinConfigs() {
         return new ArrayList<>(Arrays.asList(
-                "mixins/mixins." + this.getModuleId() + ".json"
+            "mixins/mixins." + this.getModuleId() + ".json"
         ));
     }
 
