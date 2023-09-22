158b462b85b3cc14429d475be261c27e78a2347e
Toray-Life <stypmabichek@gmail.com>
2023-09-22 14:18:56 +0400

    Optimize imports

--- src/main/java/toraylife/mappetextras/modules/scripting/mixins/MixinScriptServer.java
+++ src/main/java/toraylife/mappetextras/modules/scripting/mixins/MixinScriptServer.java
@@ -1,15 +1,9 @@
 package toraylife.mappetextras.modules.scripting.mixins;
 
-import mchorse.mappet.api.scripts.code.ScriptFactory;
 import mchorse.mappet.api.scripts.code.ScriptServer;
-import mchorse.mappet.api.scripts.code.entities.ScriptPlayer;
 import mchorse.mappet.api.scripts.user.IScriptServer;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.management.UserListOps;
 import org.spongepowered.asm.mixin.Mixin;
-import toraylife.mappetextras.modules.scripting.mixins.helpers.MixinScriptFactoryHelper;
 
 @Mixin(value = ScriptServer.class, remap = false)
 public abstract class MixinScriptServer {
