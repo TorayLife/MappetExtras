58afd9edb40bd2573efa564a5bbff2ee2fc56cd4
Toray-Life <stypmabichek@gmail.com>
2023-09-28 12:00:08 +0400

    Add temp cast

--- src/main/java/toraylife/mappetextras/modules/scripting/scripts/code/ScriptEmitterTileEntity.java
+++ src/main/java/toraylife/mappetextras/modules/scripting/scripts/code/ScriptEmitterTileEntity.java
@@ -3,6 +3,7 @@ package toraylife.mappetextras.modules.scripting.scripts.code;
 import mchorse.mappet.api.scripts.code.blocks.ScriptTileEntity;
 import mchorse.mappet.network.common.blocks.PacketEditEmitter;
 import mchorse.mappet.tile.TileEmitter;
+import toraylife.mappetextras.modules.scripting.scripts.user.IScriptChecker;
 import toraylife.mappetextras.modules.scripting.scripts.user.IScriptEmitterTileEntity;
 
 public class ScriptEmitterTileEntity extends ScriptTileEntity implements IScriptEmitterTileEntity {
@@ -15,12 +16,12 @@ public class ScriptEmitterTileEntity extends ScriptTileEntity implements IScript
         return new ScriptChecker(((TileEmitter)this.getMinecraftTileEntity()).getChecker());
     }
 
-    public void setChecker(ScriptChecker checker) {
+    public void setChecker(IScriptChecker checker) {
         TileEmitter tile = (TileEmitter) this.getMinecraftTileEntity();
 
         /* TODO: Rewrite with access transformer */
         PacketEditEmitter packetEditEmitter = new PacketEditEmitter(tile);
-        packetEditEmitter.checker = checker.checker.toNBT();
+        packetEditEmitter.checker = ((ScriptChecker) checker).checker.toNBT();
         tile.setExpression(packetEditEmitter);
     }
 
