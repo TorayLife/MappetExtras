d864f66292cc0d09a1f5457410786ffdf1167418
Toray-Life <stypmabichek@gmail.com>
2023-09-28 21:45:52 +0400

    Fix version message issue

--- src/main/java/toraylife/mappetextras/modules/main/client/gui/ValueVersionCheck.java
+++ src/main/java/toraylife/mappetextras/modules/main/client/gui/ValueVersionCheck.java
@@ -1,21 +1,17 @@
 package toraylife.mappetextras.modules.main.client.gui;
 
-import mchorse.mappet.ClientProxy;
-import mchorse.mappet.client.gui.utils.text.GuiText;
 import mchorse.mclib.client.gui.framework.elements.GuiElement;
 import mchorse.mclib.client.gui.framework.elements.buttons.GuiButtonElement;
-import mchorse.mclib.client.gui.utils.Elements;
-import mchorse.mclib.client.gui.utils.GuiUtils;
 import mchorse.mclib.client.gui.utils.keys.IKey;
 import mchorse.mclib.config.gui.GuiConfigPanel;
 import mchorse.mclib.config.values.ValueGUI;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.resources.I18n;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
-import toraylife.mappetextras.CommonProxy;
+import toraylife.mappetextras.modules.main.VersionChecker;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class ValueVersionCheck extends ValueGUI
@@ -29,12 +25,12 @@ public class ValueVersionCheck extends ValueGUI
     @SideOnly(Side.CLIENT)
     public List<GuiElement> getFields(Minecraft mc, GuiConfigPanel config)
     {
-        return Arrays.asList(
-            new GuiButtonElement(
-                mc,
-                IKey.lang("mappetextras.config.main_module.show_version_update_message"),
-                (b) -> CommonProxy.eventHandler.sendUpdateMessage(true)
-            ).marginTop(6)
+        return Collections.singletonList(
+                new GuiButtonElement(
+                        mc,
+                        IKey.lang("mappetextras.config.main_module.show_version_update_message"),
+                        (b) -> VersionChecker.sendMessage(mc.player, true)
+                ).marginTop(6)
         );
     }
 }
