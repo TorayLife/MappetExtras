4844d0d09874dbf3f587edc3ab6d9a860276d5df
Toray-Life <stypmabichek@gmail.com>
2023-10-06 00:10:14 +0400

    Add trigger methods for ScriptNPC

--- src/main/java/toraylife/mappetextras/modules/scripting/mixins/MixinScriptNpc.java
+++ src/main/java/toraylife/mappetextras/modules/scripting/mixins/MixinScriptNpc.java
@@ -101,4 +101,148 @@ public abstract class MixinScriptNpc extends ScriptEntity<EntityNpc> {
     public IScriptTrigger getPatrolTrigger(int index) {
         return new ScriptTrigger(this.entity.getState().patrolTriggers.get(index));
     }
+
+    /**
+     * Gets the initialization trigger.
+     *
+     * @return The initialization trigger
+     */
+    public IScriptTrigger getInitializationTrigger() {
+        return new ScriptTrigger(this.entity.getState().triggerInitialize);
+    }
+
+    /**
+     * Sets the initialization trigger.
+     *
+     * @param trigger The new initialization trigger
+     */
+    public void setInitializationTrigger(IScriptTrigger trigger) {
+        this.entity.getState().triggerInitialize = trigger.getTrigger();
+    }
+
+    /**
+     * Gets the interaction trigger.
+     *
+     * @return The interaction trigger
+     */
+    public IScriptTrigger getInteractionTrigger() {
+        return new ScriptTrigger(this.entity.getState().triggerInteract);
+    }
+
+    /**
+     * Sets the interaction trigger.
+     *
+     * @param trigger The new interaction trigger
+     */
+    public void setInteractionTrigger(IScriptTrigger trigger) {
+        this.entity.getState().triggerInteract = trigger.getTrigger();
+    }
+
+    /**
+     * Gets the damaged trigger.
+     *
+     * @return The damaged trigger
+     */
+    public IScriptTrigger getDamagedTrigger() {
+        return new ScriptTrigger(this.entity.getState().triggerDamaged);
+    }
+
+    /**
+     * Sets the damaged trigger.
+     *
+     * @param trigger The new damaged trigger
+     */
+    public void setDamagedTrigger(IScriptTrigger trigger) {
+        this.entity.getState().triggerDamaged = trigger.getTrigger();
+    }
+
+    /**
+     * Gets the death trigger.
+     *
+     * @return The death trigger
+     */
+    public IScriptTrigger getDeathTrigger() {
+        return new ScriptTrigger(this.entity.getState().triggerDied);
+    }
+
+    /**
+     * Sets the death trigger.
+     *
+     * @param trigger The new death trigger
+     */
+    public void setDeathTrigger(IScriptTrigger trigger) {
+        this.entity.getState().triggerDied = trigger.getTrigger();
+    }
+
+    /**
+     * Gets the tick trigger.
+     *
+     * @return The tick trigger
+     */
+    public IScriptTrigger getTickTrigger() {
+        return new ScriptTrigger(this.entity.getState().triggerTick);
+    }
+
+    /**
+     * Sets the tick trigger.
+     *
+     * @param trigger The new tick trigger
+     */
+    public void setTickTrigger(IScriptTrigger trigger) {
+        this.entity.getState().triggerTick = trigger.getTrigger();
+    }
+
+    /**
+     * Gets the target trigger.
+     *
+     * @return The target trigger
+     */
+    public IScriptTrigger getTargetTrigger() {
+        return new ScriptTrigger(this.entity.getState().triggerTarget);
+    }
+
+    /**
+     * Sets the target trigger.
+     *
+     * @param trigger The new target trigger
+     */
+    public void setTargetTrigger(IScriptTrigger trigger) {
+        this.entity.getState().triggerTarget = trigger.getTrigger();
+    }
+
+    /**
+     * Gets the collision trigger.
+     *
+     * @return The collision trigger
+     */
+    public IScriptTrigger getCollisionTrigger() {
+        return new ScriptTrigger(this.entity.getState().triggerEntityCollision);
+    }
+
+    /**
+     * Sets the collision trigger.
+     *
+     * @param trigger The new collision trigger
+     */
+    public void setCollisionTrigger(IScriptTrigger trigger) {
+        this.entity.getState().triggerEntityCollision = trigger.getTrigger();
+    }
+
+    /**
+     * Gets the respawn trigger.
+     *
+     * @return The respawn trigger
+     */
+    public IScriptTrigger getRespawnTrigger() {
+        return new ScriptTrigger(this.entity.getState().triggerRespawn);
+    }
+
+    /**
+     * Sets the respawn trigger.
+     *
+     * @param trigger The new respawn trigger
+     */
+    public void setRespawnTrigger(IScriptTrigger trigger) {
+        this.entity.getState().triggerRespawn = trigger.getTrigger();
+    }
 }
