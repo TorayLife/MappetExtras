4226a9fe9b6bed4c560c1d95288c0a48f15d36f5
Toray-Life <stypmabichek@gmail.com>
2023-10-07 15:35:28 +0400

    Fixed bug where overloadings were not appearing in documentation

---
+++ src/main/java/toraylife/mappetextras/modules/main/documentation/PreciseDocClass.java
@@ -0,0 +1,8 @@
+package toraylife.mappetextras.modules.main.documentation;
+
+import mchorse.mappet.client.gui.scripts.utils.documentation.DocMethod;
+
+public interface PreciseDocClass {
+
+    DocMethod getExactMethod(String name);
+}
