93d61a01d47ce6f392a58b83509754ace5b177dc
Toray-Life <stypmabichek@gmail.com>
2023-10-14 23:56:14 +0400

    Add replace item ability to filled soulstone

--- src/main/java/toraylife/mappetextras/modules/utils/items/ItemNpcSoulstoneFilled.java
+++ src/main/java/toraylife/mappetextras/modules/utils/items/ItemNpcSoulstoneFilled.java
@@ -8,6 +8,7 @@ import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
@@ -15,6 +16,7 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import toraylife.mappetextras.MappetExtras;
 
 import javax.annotation.Nullable;
 import java.util.List;
@@ -49,5 +51,27 @@ public class ItemNpcSoulstoneFilled extends Item {
         }
 
         return stack.getItem() == Mappet.npcTool ? EnumActionResult.SUCCESS : super.onItemUse(player, worldIn, pos, hand, facing, hitX, hitY, hitZ);
+    }
+
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
+        if (playerIn.isCreative() && playerIn.isSneaking()) {
+            if (worldIn.isRemote) {
+                return super.onItemRightClick(worldIn, playerIn, handIn);
+            }
+
+            if (this.replaceItem(playerIn, playerIn.getHeldItem(handIn), handIn)) {
+                return new ActionResult(EnumActionResult.SUCCESS, playerIn.getHeldItem(handIn));
+            }
+        }
+
+        return super.onItemRightClick(worldIn, playerIn, handIn);
+    }
+
+    private boolean replaceItem(EntityPlayer player, ItemStack stack, EnumHand hand) {
+        ItemStack npcPicker = new ItemStack(MappetExtras.npcPicker);
+
+        npcPicker.setTagCompound(stack.getTagCompound());
+        player.setHeldItem(hand, npcPicker);
+        return true;
     }
 }
