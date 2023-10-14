419819cfeb4d51ef9f12dd1021d2c2080c68a432
Toray-Life <stypmabichek@gmail.com>
2023-10-14 23:31:18 +0400

    Add holidays textures (by _hacker__demon_)

--- src/main/java/toraylife/mappetextras/events/RegisterHandler.java
+++ src/main/java/toraylife/mappetextras/events/RegisterHandler.java
@@ -9,30 +9,93 @@ import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import toraylife.mappetextras.MappetExtras;
 import toraylife.mappetextras.modules.utils.items.ItemNpcPicker;
 import toraylife.mappetextras.modules.utils.items.ItemNpcSoulstoneEmpty;
 import toraylife.mappetextras.modules.utils.items.ItemNpcSoulstoneFilled;
 
+import java.util.Calendar;
+
 public class RegisterHandler {
-    public RegisterHandler() {
-    }
-
-    ModelResourceLocation npcPickerTexture = new ModelResourceLocation("mappetextras:npc_picker", "inventory");
-    ModelResourceLocation npcSoulstoneEmptyTexture = new ModelResourceLocation("mappetextras:npc_soulstone_empty", "inventory");
-    ModelResourceLocation npcSoulstoneFilledTexture = new ModelResourceLocation("mappetextras:npc_soulstone_filled", "inventory");
-
     @SubscribeEvent
     public void onItemsRegister(RegistryEvent.Register<Item> event) {
-        event.getRegistry().register(MappetExtras.npcPicker = ((Item) (new ItemNpcPicker()).setRegistryName(new ResourceLocation("mappetextras", "npc_picker"))).setUnlocalizedName("mappetextras.npc_picker"));
-        event.getRegistry().register(MappetExtras.npcSoulstoneEmpty = ((Item) (new ItemNpcSoulstoneEmpty()).setRegistryName(new ResourceLocation("mappetextras", "npc_soulstone_empty"))).setUnlocalizedName("mappetextras.npc_soulstone"));
-        event.getRegistry().register(MappetExtras.npcSoulstoneFilled = ((Item) (new ItemNpcSoulstoneFilled()).setRegistryName(new ResourceLocation("mappetextras", "npc_soulstone_filled"))).setUnlocalizedName("mappetextras.npc_soulstone"));
+        event.getRegistry().register(MappetExtras.npcPicker = (new ItemNpcPicker()).setRegistryName(new ResourceLocation("mappetextras", "npc_picker")).setUnlocalizedName("mappetextras.npc_picker"));
+        event.getRegistry().register(MappetExtras.npcSoulstoneEmpty = (new ItemNpcSoulstoneEmpty()).setRegistryName(new ResourceLocation("mappetextras", "npc_soulstone_empty")).setUnlocalizedName("mappetextras.npc_soulstone"));
+        event.getRegistry().register(MappetExtras.npcSoulstoneFilled = (new ItemNpcSoulstoneFilled()).setRegistryName(new ResourceLocation("mappetextras", "npc_soulstone_filled")).setUnlocalizedName("mappetextras.npc_soulstone"));
     }
 
     @SubscribeEvent
     @SideOnly(Side.CLIENT)
     public void onModelRegistry(ModelRegistryEvent event) {
-        ModelLoader.setCustomModelResourceLocation(MappetExtras.npcPicker, 0, npcPickerTexture);
-        ModelLoader.setCustomModelResourceLocation(MappetExtras.npcSoulstoneEmpty, 0, npcSoulstoneEmptyTexture);
-        ModelLoader.setCustomModelResourceLocation(MappetExtras.npcSoulstoneFilled, 0, npcSoulstoneFilledTexture);
+        ModelLoader.setCustomModelResourceLocation(MappetExtras.npcPicker, 0, getNpcToolTexture("npc_picker"));
+        ModelLoader.setCustomModelResourceLocation(MappetExtras.npcSoulstoneEmpty, 0, getNpcToolTexture("npc_soulstone_empty"));
+        ModelLoader.setCustomModelResourceLocation(MappetExtras.npcSoulstoneFilled, 0, getNpcToolTexture("npc_soulstone_filled"));
+    }
+
+    public ModelResourceLocation getNpcToolTexture(String toolname) {
+        String postfix = "";
+        Calendar calendar = Calendar.getInstance();
+
+        if (isWinter(calendar)) {
+            postfix = "_winter";
+        }
+
+        if (isChristmas(calendar)) {
+            postfix = "_christmas";
+        } else if (isEaster(calendar)) {
+            postfix = "_easter";
+        } else if (isAprilFoolsDay(calendar)) {
+            postfix = "_april";
+        } else if (isHalloween(calendar)) {
+            postfix = "_halloween";
+        }
+
+        return new ModelResourceLocation(MappetExtras.MOD_ID + ":" + toolname + "/" + toolname + postfix, "inventory");
+    }
+
+    public boolean isChristmas(Calendar calendar) {
+        return calendar.get(Calendar.MONTH) == Calendar.DECEMBER && calendar.get(Calendar.DATE) >= 24 && calendar.get(Calendar.DATE) <= 26;
+    }
+
+    public boolean isAprilFoolsDay(Calendar calendar) {
+        return calendar.get(Calendar.MONTH) == Calendar.APRIL && calendar.get(Calendar.DATE) <= 2;
+    }
+
+    public boolean isWinter(Calendar calendar) {
+        int month = calendar.get(Calendar.MONTH);
+
+        return month == Calendar.DECEMBER || month == Calendar.JANUARY || month == Calendar.FEBRUARY;
+    }
+
+    public boolean isEaster(Calendar calendar) {
+        Calendar easterDate = getEasterDate(calendar.get(Calendar.YEAR));
+
+        return calendar.get(Calendar.MONTH) == easterDate.get(Calendar.MONTH) && calendar.get(Calendar.DATE) == easterDate.get(Calendar.DATE);
+    }
+
+    public boolean isHalloween(Calendar calendar) {
+        return calendar.get(Calendar.MONTH) == Calendar.OCTOBER && calendar.get(Calendar.DATE) >= 24;
+    }
+
+    public Calendar getEasterDate(int year) {
+        int a = year % 19;
+        int b = year / 100;
+        int c = year % 100;
+        int d = b / 4;
+        int e = b % 4;
+        int f = (b + 8) / 25;
+        int g = (b - f + 1) / 3;
+        int h = (19 * a + b - d - g + 15) % 30;
+        int i = c / 4;
+        int k = c % 4;
+        int l = (32 + 2 * e + 2 * i - h - k) % 7;
+        int month = (h + l + 114) / 31;
+        int day = (h + l + 114) % 31;
+
+        Calendar calendar = Calendar.getInstance();
+
+        calendar.set(year, month - 1, day + 1);
+
+        return calendar;
     }
 }
