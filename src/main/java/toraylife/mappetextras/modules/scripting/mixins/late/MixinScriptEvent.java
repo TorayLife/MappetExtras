c6a9d4ef94925ed4bd91d5e90ff3e516fffc9d8b
mines <jenya-vev@list.ru>
2023-12-04 18:22:40 +0300

    add stopScript()

--- src/main/java/toraylife/mappetextras/modules/scripting/mixins/late/MixinScriptEvent.java
+++ src/main/java/toraylife/mappetextras/modules/scripting/mixins/late/MixinScriptEvent.java
@@ -3,12 +3,19 @@ package toraylife.mappetextras.modules.scripting.mixins.late;
 import mchorse.mappet.CommonProxy;
 import mchorse.mappet.api.scripts.code.ScriptEvent;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import toraylife.mappetextras.modules.main.mixins.utils.MixinTargetName;
 
 @Mixin(value = ScriptEvent.class, remap = false)
 @MixinTargetName("mchorse.mappet.api.scripts.user.IScriptEvent")
 public abstract class MixinScriptEvent {
+    @Shadow
+    private String script;
     public void stopScript(String script){
         CommonProxy.eventHandler.removeExecutables(script);
+    }
+
+    public void stopScript(){
+        CommonProxy.eventHandler.removeExecutables(this.script);
     }
 }
