41ccde98fc2d63f041aef39a303573deaba402b7
mines <jenya-vev@list.ru>
2023-12-04 18:07:50 +0300

    this.uniqueId = ""

--- src/main/java/toraylife/mappetextras/modules/client/network/PacketClientData.java
+++ src/main/java/toraylife/mappetextras/modules/client/network/PacketClientData.java
@@ -50,7 +50,7 @@ public class PacketClientData implements IMessage {
         this.access = access;
         this.nbtTagCompound = nbtTagCompound;
         this.nbtData = new NBTTagCompound();
-        this.uniqueId = "uniqueId";
+        this.uniqueId = "";
     }
 
     public PacketClientData(ClientData type, AccessType access, NBTTagCompound nbtTagCompound, UUID uniqueId) {
