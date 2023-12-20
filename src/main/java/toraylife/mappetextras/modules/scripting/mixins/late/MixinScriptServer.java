2b1a5135d80b24ae748730d1e366ca5de30ed53f
Toray-Life <stypmabichek@gmail.com>
2023-12-20 15:12:01 +0400

    Update CHANGELOG, bump version

--- src/main/java/toraylife/mappetextras/modules/scripting/mixins/late/MixinScriptServer.java
+++ src/main/java/toraylife/mappetextras/modules/scripting/mixins/late/MixinScriptServer.java
@@ -1,11 +1,12 @@
 package toraylife.mappetextras.modules.scripting.mixins.late;
 
+import mchorse.mappet.Mappet;
 import mchorse.mappet.api.scripts.code.ScriptServer;
-import mchorse.mappet.api.scripts.code.entities.ScriptPlayer;
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import toraylife.mappetextras.modules.main.mixins.utils.MixinTargetName;
+import toraylife.mappetextras.modules.scripting.scripts.code.triggers.ScriptTrigger;
 
 import java.util.Arrays;
 import java.util.List;
