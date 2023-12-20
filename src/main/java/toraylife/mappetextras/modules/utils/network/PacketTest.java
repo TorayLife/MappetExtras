42ccf7c544ec7a73154afe6bc1e8e063acd99585
mines <jenya-vev@list.ru>
2023-12-20 21:39:19 +0300

    FIX packets

--- src/main/java/toraylife/mappetextras/modules/utils/network/PacketTest.java
+++ src/main/java/toraylife/mappetextras/modules/utils/network/PacketTest.java
@@ -4,6 +4,8 @@ import io.netty.buffer.ByteBuf;
 import mchorse.mclib.network.ClientMessageHandler;
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class PacketTest implements IMessage {
 
@@ -22,6 +24,7 @@ public class PacketTest implements IMessage {
 
     public static class ClientHandlerTest extends ClientMessageHandler<PacketTest> {
         @Override
+        @SideOnly(Side.CLIENT)
         public void run(EntityPlayerSP player, PacketTest message) {
             player.sendChatMessage("You fool!");
         }
