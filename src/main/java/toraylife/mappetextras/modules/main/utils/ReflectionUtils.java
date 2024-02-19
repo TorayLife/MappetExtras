77a9965b9103c55081b361addb8458f7919d5bc8
TorayLife <61179430+TorayLife@users.noreply.github.com>
2024-02-19 15:16:21 +0400

    feature/play_animation (#38)
    * ✨ feat: add method for playing morph action animations
       ✨ feat: add utility method for reflection in main module and refactor morph retrieval logic
    * 🔥 refactor: remove unused imports and mixins

---
+++ src/main/java/toraylife/mappetextras/modules/main/utils/ReflectionUtils.java
@@ -0,0 +1,36 @@
+package toraylife.mappetextras.modules.main.utils;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+public class ReflectionUtils {
+
+    public static Method getMethod(Class<?> clazz, String methodName, Object... args) {
+        try {
+            Method method = clazz.getMethod(methodName, Arrays.stream(args).map(Object::getClass).toArray(Class[]::new));
+            method.setAccessible(true);
+            return method;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static Method getMethod(Class<?> clazz, String methodName) {
+        return getMethod(clazz, methodName, new Object[0]);
+    }
+
+    public static Object getAndInvokeMethod(Class<?> clazz, String methodName, Object target, Object... args) {
+        try {
+            Method method = getMethod(clazz, methodName, args);
+            return method.invoke(target, args);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static Object getAndInvokeMethod(Class<?> clazz, String methodName, Object target) {
+        return getAndInvokeMethod(clazz, methodName, target, new Object[0]);
+    }
+}
