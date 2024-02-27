d351aaddb0a7f409e160c0db3af70686aafb8c43
llama <jenya-vev@list.ru>
2024-02-27 19:27:16 +0300

    fixed MixinFolderManager

--- src/main/java/toraylife/mappetextras/modules/scripting/mixins/late/MixinScriptManager.java
+++ src/main/java/toraylife/mappetextras/modules/scripting/mixins/late/MixinScriptManager.java
@@ -1,7 +1,11 @@
 package toraylife.mappetextras.modules.scripting.mixins.late;
 
+import mchorse.mappet.api.scripts.Script;
+import mchorse.mappet.api.scripts.ScriptManager;
+import mchorse.mappet.api.utils.manager.BaseManager;
 import mchorse.mappet.api.utils.manager.FolderManager;
 import mchorse.mclib.config.values.ValueString;
+import net.minecraft.nbt.NBTTagCompound;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,9 +17,11 @@ import toraylife.mappetextras.modules.scripting.ScriptingModule;
 import java.io.File;
 import java.util.Objects;
 
-@Mixin(value = FolderManager.class, remap = false)
-public class MixinFolderManager {
-    @Shadow protected File folder;
+@Mixin(value = ScriptManager.class, remap = false)
+public abstract class MixinScriptManager extends BaseManager<Script> {
+    public MixinScriptManager(File folder) {
+        super(folder);
+    }
 
     @Inject(method = "<init>", at = @At("TAIL"))
     public void init(File folder, CallbackInfo ci) {
